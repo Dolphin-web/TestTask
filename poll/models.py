596dfafd7b0cc8092ac13885e721188191b17f06
Dolphin-web <64734454+Dolphin-web@users.noreply.github.com>
2021-10-07 20:35:10 +0600

    Update models.py

--- poll/models.py
+++ poll/models.py
@@ -32,7 +32,6 @@ class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.DO_NOTHING)
 	title = models.CharField(max_length=4096, default ='text')
 	lock_other = models.BooleanField(default=False)
-	on_text = models.BooleanField(default=False)
 
 	def __str__(self):
 		return self.title
